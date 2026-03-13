fb040ecd08b3eacd99b9da4ac4c4e83a6bb339e6
Skylar <ssbougher@gmail.com>
2026-03-13 13:59:00 +0000

    Added pages and navigation

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
     res.send("<h1>Welcome to My Website</h1>")
 })
+app.get('/about', (req, res) => {
+    res.send("<h1>About me</h1><p>This page describes me<p>")
+})
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
+})
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/public/about.html')
+})
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/public/contact.html')
 })
